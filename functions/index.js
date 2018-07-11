2bd5d714b5c094fd383a76095d781b0f2a738b78
Abhishek <singhsterabhi@gmail.com>
2018-07-12 03:13:19 +0530

    total ui overhaul & added cloud function

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.addNewUser = functions.auth.user().onCreate((user) => {
+    console.log(user);
+    console.log(user.email);
+    console.log(user.displayName);
+    console.log(user.uid);
+    data = {
+        name: user.displayName,
+        email: user.email,
+        admin: "false"
+    };
+    console.log(data);
+    admin.database().ref(`/users/${user.uid}`).set(data);
+});
